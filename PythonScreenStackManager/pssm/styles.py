ae862895c818711fb1d0352c491b03b9bb9fee16
Sander <sanderderidder@hotmail.nl>
2024-12-16 10:16:19 +0100

    Detached SHORTHAND_COLORS from add_colors

--- PythonScreenStackManager/pssm/styles.py
+++ PythonScreenStackManager/pssm/styles.py
@@ -16,18 +16,6 @@ logger = logging.getLogger(__name__)
 
 SHORTHAND_COLORS = PSSM_COLORS.copy()
 
-def add_colors(**kwargs: ColorType):
-    for col_name, color in kwargs.items():
-        if col_name in SHORTHAND_COLORS:
-            logger.error(f"{col_name} is already registered as a shorthand color")
-            continue
-        if not tools.is_valid_Color(color):
-            logger.error(f"color {color} with shorthand {col_name} is not a valid color value")
-            continue
-        SHORTHAND_COLORS[col_name] = color
-
-
-
 class Style:
     """Handles styling and theming of Elements
     _summary_
@@ -39,6 +27,11 @@ class Style:
     """
 
     screen: "PSSMScreen"
+    _color_shorthands: dict[str,ColorType] = {}
+
+    @classproperty
+    def shorthand_colors(cls):
+        return SHORTHAND_COLORS | cls._color_shorthands
 
     @classmethod
     def get_color(cls, value: ColorType, colormode: str = "screen-image"):
@@ -47,8 +40,8 @@ class Style:
         elif colormode == "screen":
             colormode = cls.screen.colorMode
         
-        if isinstance(value,str) and value.lower() in SHORTHAND_COLORS:
-            return SHORTHAND_COLORS[value.lower()]
+        if isinstance(value,str) and value.lower() in cls.shorthand_colors:
+            return cls.shorthand_colors[value.lower()]
         else:
             try:
                 return tools.get_Color(value,colormode)
@@ -57,8 +50,8 @@ class Style:
             
     @classmethod
     def contrast_color(cls, value, mode):
-        if isinstance(value,str) and value.lower() in SHORTHAND_COLORS:
-            value = SHORTHAND_COLORS[value.lower()]
+        if isinstance(value,str) and value.lower() in cls.shorthand_colors:
+            value = cls.shorthand_colors[value.lower()]
         
         return tools.contrast_color(value, mode)
             
@@ -80,12 +73,23 @@ class Style:
         bool
             Whether the color is valid
         """        
-        if isinstance(value,str) and value.lower() in SHORTHAND_COLORS:
+        if isinstance(value,str) and value.lower() in cls.shorthand_colors:
             return True
         else:
             return tools.is_valid_Color(value)
         return
 
+    @classmethod
+    def add_color_shorthand(cls, **kwargs: ColorType):
+        shorthands = cls.shorthand_colors
+        for col_name, color in kwargs.items():
+            if col_name in shorthands:
+                logger.error(f"{col_name} is already registered as a shorthand color")
+                continue
+            if not tools.is_valid_Color(color):
+                logger.error(f"color {color} with shorthand {col_name} is not a valid color value")
+                continue
+            cls._color_shorthands[col_name] = color
     ##Setting up a color property:
     ##pass as style (identifier)-color-subclass-class
 
