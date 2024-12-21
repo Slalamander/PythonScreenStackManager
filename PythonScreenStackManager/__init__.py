4dd0a8bf9b7337006f01ce440e936c2a309c2d28
Sander <sanderderidder@hotmail.nl>
2024-12-21 21:15:11 +0100

    bump dev version

--- PythonScreenStackManager/__init__.py
+++ PythonScreenStackManager/__init__.py
@@ -4,7 +4,7 @@
     Rewritten to use asyncio by Slalamander, among other changes
 """
 
-__version__ = "0.3.6.dev1"
+__version__ = "0.3.6.dev2"
 "PythonScreenStackManager version. For now the s is in front to indicate it is the version continued by Slalamander"
 
 import __main__
