937b98fbf15ba0b6495e8ee578444048a6c78eb0
Sander <sanderderidder@hotmail.nl>
2025-01-20 11:49:50 +0100

    feat(pssm): :sparkles: Create TriggerCondition class to quickly acquire Condition locks

--- PythonScreenStackManager/pssm/util.py
+++ PythonScreenStackManager/pssm/util.py
@@ -33,3 +33,40 @@ class PSSMEventLoopPolicy(asyncio.DefaultEventLoopPolicy):
         # loop = super().get_event_loop()
         # Do something with loop ...
         return self._screen.mainLoop
+    
+class TriggerCondition(asyncio.Condition):
+    """Subclass of asyncio.Condition with convenience methods
+
+    This class provides the ``trigger_all`` and ``trigger`` functions.
+    These are simple async functions that call ``notify_all`` and ``notify`` respectively,
+    but with the added convenience of acquiring the lock first.
+
+    This makes it quicker to simply notify something without the need to do anything else in the locked state,
+    as it omits the need to write the line to acquire the lock each time.
+    """    
+
+    async def trigger_all(self):
+        """Acquire the trigger's lock and notify all waiters
+        """        
+        async with self:
+            self.notify_all()
+        return
+    
+    async def trigger(self, n = 1):
+        """Acquire the trigger's lock and notify <n> waiters
+        """  
+        async with self:
+            self.notify(n)
+
+    async def await_trigger(self):
+        """Acquire the trigger's lock and wait to be notified
+        """  
+        async with self:
+            await self.wait()
+
+    async def await_for_trigger(self, predicate):
+        """Acquire the trigger's lock and wait to be notified AND for the predicate to evaluate to ``True``.
+        """  
+        async with self:
+            res = await self.wait_for(predicate)
+        return res
