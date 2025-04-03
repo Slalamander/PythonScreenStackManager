26b6399a3ebc4f887fadb7069b943cf4a0648e3e
Sander <sanderderidder@hotmail.nl>
2025-04-03 15:00:19 +0200

    refactor(platform): ♻️ Rename a variable to correct name

--- PythonScreenStackManager/devices/windowed.py
+++ PythonScreenStackManager/devices/windowed.py
@@ -652,12 +652,12 @@ class Backlight(BaseBacklight):
 
     @trigger_condition
     async def __transition(self, brightness : int, transition: float):
-        if not self.transitionTask.done():
-            self.transitionTask.cancel("New transition received")
+        if not self._transitionTask.done():
+            self._transitionTask.cancel("New transition received")
 
-        self.transitionTask = asyncio.create_task( self.__async_transition(brightness, transition))
+        self._transitionTask = asyncio.create_task( self.__async_transition(brightness, transition))
         try:
-            await self.transitionTask #@IgnoreException
+            await self._transitionTask #@IgnoreException
         except asyncio.CancelledError as exce:
             _LOGGER.debug(f"Transition task to {brightness}% in {transition} seconds was cancelled")
         
